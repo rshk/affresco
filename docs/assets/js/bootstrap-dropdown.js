9285d9f2316551c96252c16d0a382fabde9a8602
Samuele Santi <redshadow@hackzine.org>
2013-04-26 18:23:32 +0200

    Improved documentation, rebuilt all the js

--- docs/assets/js/bootstrap-dropdown.js
+++ docs/assets/js/bootstrap-dropdown.js
@@ -1,3 +1,13 @@
+/**
+* Affresco.js v0.1 by Samuele Santi
+* Copyright 2013 Samuele Santi
+*
+* Original work:
+* Bootstrap.js v2.3.1 by @fat & @mdo
+* Copyright 2012 Twitter, Inc.
+*
+* License: http://www.apache.org/licenses/LICENSE-2.0.txt
+*/
 /* ============================================================
  * bootstrap-dropdown.js v2.3.1
  * http://twitter.github.com/bootstrap/javascript.html#dropdowns
